4a09df0b4471a2af1e80194b2246dc5c4d1bd54a
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2022-09-18 15:04:50 +0900

    Chapterクラスを作成

--- ffprove.rb
+++ ffprove.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
+require_relative './chapter'
 require 'json'
 
 module FFProve
   def self.chapters(file_path, base_path: '/mnt/d/BDRipping/')
     ffprove_output = JSON.parse(`ffprobe #{file_path} -of json -show_chapters -loglevel quiet`,
                                 symbolize_names: true)
-    pp ffprove_output.dig(:chapters)
+    ffprove_output[:chapters].map do |chapter|
+      Chapter.new(
+        start_at: chapter[:start],
+        end_at: chapter[:end],
+        time_base: chapter[:time_base],
+      )
+    end
   end
 end
 
