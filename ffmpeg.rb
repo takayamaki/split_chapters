58bd2850d5a669d65996d1aa728f7aff10da144f
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2022-12-16 18:36:18 +0900

    ffmpecの2passエンコードのコマンドを出力できる

---
+++ ffmpeg.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'pathname'
+
+module FFMpeg
+  BASE_COMMAND = 'bin\ffmpeg.exe -y -hide_banner -loglevel error -stats -hwaccel qsv'
+  ENCODE_OPTIONS = '-vcodec libx264 -preset veryslow -b:v 8192k -acodec aac -b:a 128k -map 0:0 -map 0:1 -pix_fmt yuv420p'
+
+  class << self
+    def output_commands(video)
+      seq_number_digits = video.chapters.length.to_s.length
+      video.chapters.each.with_index(1).flat_map do |chapter, seq_number|
+        formatted_seq_number = format("%0#{seq_number_digits}d", seq_number)
+        [
+          two_pass_encode_1st_pass(chapter, video.file_path),
+          two_pass_encode_2nd_pass(chapter, video.file_path, formatted_seq_number)
+        ]
+      end
+    end
+
+    def two_pass_encode_1st_pass(chapter, path)
+      [
+        BASE_COMMAND,
+        "-ss #{chapter.start_at_integer_part}",
+        "-i #{convert_to_win_path(path)}",
+        "-ss #{chapter.start_at_fractional_part.to_f}",
+        "-t #{chapter.duration.to_f}",
+        ENCODE_OPTIONS,
+        '-pass 1 -f null',
+        'nul'
+      ].join(' ')
+    end
+
+    def two_pass_encode_2nd_pass(chapter, path, seq_number)
+      [
+        BASE_COMMAND,
+        "-ss #{chapter.start_at_integer_part}",
+        "-i #{convert_to_win_path(path)}",
+        "-ss #{chapter.start_at_fractional_part.to_f}",
+        "-t #{chapter.duration.to_f}",
+        ENCODE_OPTIONS,
+        '-pass 2 -movflags +faststart',
+        convert_to_win_path(build_output_path(path, seq_number))
+      ].join(' ')
+    end
+
+
+    def build_output_path(src_path, seq_number)
+      src_path = Pathname(src_path)
+
+      [
+        src_path.dirname,
+        '/',
+        "#{src_path.basename(src_path.extname)}_#{seq_number}",
+        '.mp4'
+      ].join
+    end
+
+    def convert_to_win_path(path)
+      path.sub(%r{/mnt/([a-z])/}) { "#{Regexp.last_match(1).upcase}:\\" }.gsub('/', '\\')
+    end
+  end
+end
--- split_chapters.rb
+++ split_chapters.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 require_relative './video'
+require_relative './ffmpeg'
 
 file_paths = ARGV.map do |path|
   absolute_path = File.expand_path(path)
   raise 'No such file' unless File.exist?(absolute_path)
+
   absolute_path
 end
 
@@ -11,4 +15,4 @@ videos = file_paths.map do |path|
   Video.new(path, chapters)
 end
 
-pp videos
+videos.flat_map { |video| FFMpeg.output_commands(video) }.each { |line| puts line }
