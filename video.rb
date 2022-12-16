a40665a451642c8a23f624bc3e9ff1a2a917e034
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2022-12-16 18:36:22 +0900

    rubocop

--- video.rb
+++ video.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 require_relative './ffprove'
 
 class Video
   class Chapter
-    attr_reader :start_at, :end_at
+    attr_reader :start_at, :end_at, :time_base
 
     def initialize(start_at:, end_at:, time_base:)
       @start_at = start_at
@@ -15,10 +16,11 @@ class Video
       start_at...end_at
     end
 
-    def start_at_integer_part = (start_at/time_base.denominator).to_i
-    def end_at_integer_part = (end_at/time_base.denominator).to_i
-    def start_at_fractional_part = (start_at%time_base.denominator)*time_base
-    def end_at_fractional_part = (end_at%time_base.denominator)*time_base
+    def duration = (end_at - start_at) * time_base
+    def start_at_integer_part = (start_at / time_base.denominator).to_i
+    def end_at_integer_part = (end_at / time_base.denominator).to_i
+    def start_at_fractional_part = (start_at % time_base.denominator) * time_base
+    def end_at_fractional_part = (end_at % time_base.denominator) * time_base
   end
 
   attr_reader :chapters, :file_path
@@ -29,7 +31,7 @@ class Video
       Chapter.new(
         start_at: chapter[:start],
         end_at: chapter[:end],
-        time_base: chapter[:time_base],
+        time_base: chapter[:time_base]
       )
     end
   end
